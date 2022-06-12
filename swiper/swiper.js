6577e989d86bb8eb59aa3bb859f49e1dbc9eb772
benazirmbk <kamillosan89@gmail.com>
2022-06-12 11:22:23 +0600

    second

---
+++ swiper/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.mySwiper', {
+	slidesPerView: 5,
+	slidesToScroll: 3,
+	centeredSlides: true,
+	spaceBetween: 30,
+	grabCursor: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+})
